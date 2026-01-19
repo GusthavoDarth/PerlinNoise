1df129e91fdbf7270bff8c2e43044da6e30dd1ee
gusth <gusthavo.goncalves@gmail.com>
2026-01-19 13:48:53 -0300

    fixed some problems

--- Perlin.c
+++ Perlin.c
@@ -5,17 +5,16 @@
 #include "functions.h"
 
 const int screenWidth = 800;
-const int screenHeight = 800;
-
-    
+const int screenHeight = 400;
 
 int main()
-{
+{ 
+    DisplayMode currentMode = MODE_NORMAL;
     int squareSize = 40;
     int gridHeight = (screenHeight/squareSize) +1;
     int gridWidth = (screenWidth/squareSize) +1;
+    int** screen = initMatrix(gridWidth, gridHeight);
 
-    int** screen = initMatrix(gridHeight,gridWidth);
     InitWindow(screenWidth, screenHeight, "Perlin");
     SetTargetFPS(60);
      while (!WindowShouldClose())
@@ -23,56 +22,42 @@ int main()
         BeginDrawing();
             ClearBackground(GRAY);
             
-            for(int px = 0; px < screenWidth; px++)
-            {
-                int x = px/squareSize;
-                if (x >= gridWidth-1){x = gridWidth-2;}
-                float fx = (px % squareSize) / (float)squareSize;
+            drawPerlin(currentMode, screenWidth, screenHeight, gridWidth, gridHeight, squareSize, screen);
 
-                for(int py = 0; py < screenHeight; py++)
-                {
-                    int y = py/squareSize;
-                    if (y >= gridWidth-1){y = gridWidth-2;}
-                    float fy = (py % squareSize) / (float)squareSize;
-
-                    float top = lerp(screen[y][x],screen[y][x+1],fx);
-                    float bottom = lerp(screen[y+1][x],screen[y+1][x+1],fx);
-                    float total = lerp(top,bottom,fy);
-
-                    DrawPixel(px, py, (Color){total,total,total,255});
-                }
-            }
-
-            DrawText(TextFormat("Size: %d",squareSize),10,10,20, BLUE);
+            DrawText(TextFormat("Size: %d",gridHeight),10,10,20, BLUE);
         EndDrawing();
 
         if(IsKeyPressed(KEY_SPACE)){
-            freeMatrix(screen, gridHeight);
-            screen = initMatrix(gridHeight,gridWidth);
+            freeMatrix(screen, gridWidth);
+            screen = initMatrix(gridWidth, gridHeight);
         }
         if(IsKeyPressed(KEY_UP)){
-            if(squareSize < 40)
+            if(squareSize < 80)
             {
-                int oldGridHeight = gridHeight;
+                int oldGridWidth = gridWidth;
 
                 squareSize += 1;
                 gridHeight = (screenHeight/squareSize) +1;
                 gridWidth = (screenWidth/squareSize) +1;
-                freeMatrix(screen, oldGridHeight);
-                screen = initMatrix(gridHeight, gridWidth);
+                freeMatrix(screen, oldGridWidth);
+                screen = initMatrix(gridWidth, gridHeight);
             }
         }
         if(IsKeyPressed(KEY_DOWN)){
             if(squareSize > 1)
             {
-                int oldGridHeight = gridHeight;
+                int oldGridWidth = gridWidth;
 
                 squareSize -= 1;
-                freeMatrix(screen, gridHeight);
                 gridHeight = (screenHeight/squareSize) +1;
                 gridWidth = (screenWidth/squareSize) +1;
-                screen = initMatrix(gridHeight, oldGridHeight);
+                freeMatrix(screen, oldGridWidth);
+                screen = initMatrix(gridWidth, gridHeight);
             }    
         }
+
+        if (IsKeyPressed(KEY_ONE)) currentMode = MODE_NORMAL;
+        if (IsKeyPressed(KEY_TWO)) currentMode = MODE_COLOR;
     }
+    freeMatrix(screen, gridWidth);
 }
--- functions.h
+++ functions.h
@@ -1,4 +1,57 @@
+typedef enum {
+    MODE_NORMAL,
+    MODE_COLOR
+} DisplayMode;
+
 float lerp(float a, float b, float t)
 {
 return a + t * (b - a);
+}
+
+Color viewType(DisplayMode currentMode, float total)
+{
+    Color pixelColor;
+    switch(currentMode)
+    {
+        case MODE_NORMAL:
+        pixelColor = (Color){(total),(total),(total),255};
+        break;
+        
+        case MODE_COLOR:                                    
+        float t = total / 255.0f;
+        if (t < 0.2f) pixelColor = BLUE;        // Water/deep
+        else if (t < 0.3f) pixelColor = SKYBLUE; // Shallow
+        else if (t < 0.4f) pixelColor = YELLOW;  // Sand
+        else if (t < 0.6f) pixelColor = GREEN;   // Grass
+        else if (t < 0.8f) pixelColor = BROWN;   // Dirt/Mountains
+        else pixelColor = WHITE; 
+        break;  
+    }
+    return pixelColor;
+}
+
+void drawPerlin(DisplayMode currentMode, int screenWidth, int screenHeight, int gridWidth, int gridHeight, int squareSize, int** matrix)
+{
+    Color pixelColor;
+    
+    for(int px = 0; px < screenWidth; px++)
+    {
+        int x = px/squareSize;
+        if (x >= gridWidth-1){x = gridWidth-2;}
+        float fx = (px % squareSize) / (float)squareSize;
+        for(int py = 0; py < screenHeight; py++)
+        {
+            int y = py/squareSize;
+            if (y >= gridWidth-1){y = gridWidth-2;}
+            float fy = (py % squareSize) / (float)squareSize;
+
+            float top = lerp(matrix[x][y],matrix[x+1][y],fx);
+            float bottom = lerp(matrix[x][y+1],matrix[x+1][y+1],fx);
+            float total = lerp(top,bottom,fy);
+
+            pixelColor = viewType(currentMode, total);
+
+            DrawPixel(px, py, pixelColor);
+        }
+    }
 }
